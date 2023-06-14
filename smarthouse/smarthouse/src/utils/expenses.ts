635b44b376f318e5b75ac13009dd74b9a164e749
Viniciusgsc <vinycimgsc2020@gmail.com>
2023-06-14 01:32:16 -0300

    Mobile Dashboard
    Mobile Dashboard

---
+++ smarthouse/smarthouse/src/utils/expenses.ts
@@ -0,0 +1,80 @@
+export const EXPENSES = {
+  'Sensor-Gas': [
+    {
+      id: "1",
+      label: "Tempo Ativo",
+      value: "150 horas",
+      color: "#4B0082",
+      percent: "8%"
+    },
+    {
+      id: "2",
+      label: "Vazamento",
+      value: "00%",
+      color: "#FF00FF",
+      percent: "4%"
+    },
+    {
+      id: "3",
+      label: "Consumo",
+      value: "15%",
+      color: "#576EFE",
+      percent: "74%"
+    },
+   
+ 
+  ],
+  'Sensor-Chuva': [
+    {
+      id: "1",
+      label: "Indice Pluviométrico",
+      value: "15",
+      color: "#8B0000",
+      percent: "14%"
+    },
+    {
+      id: "2",
+      label: "Tempo Ativo",
+      value: "15h",
+      color: "#1E90FF",
+      percent: "79%"
+    },
+    {
+      id: "3",
+      label: "Quantidade de MM",
+      value: "45",
+      color: "#000080",
+      percent: "6%"
+    },
+  ],
+  'Sensor-Proximidade': [
+    {
+      id: "1",
+      label: "Proximidade Indutiva",
+      value: "15%",
+      color: "#708090",
+      percent: "20%"
+    },
+    {
+      id: "2",
+      label: "Proximidade Optica",
+      value: "50%",
+      color: "#000000",
+      percent: "5%"
+    },
+    {
+      id: "3",
+      label: "Tempo Ativo",
+      value: "200h",
+      color: "#FF0000",
+      percent: "45%"
+    },
+    {
+      id: "4",
+      label: "Consumo",
+      value: "360w",
+      color: "#55C5FE",
+      percent: "29%"
+    },
+  ],
+};
---
+++ smarthouse/smarthouse/src/utils/months.ts
@@ -0,0 +1,5 @@
+export const MONTHS = [
+  { label: 'Sensor-Gas', value: '0' },
+  { label: 'Sensor-Chuva', value: '1' },
+  { label: 'Sensor-Proximidade', value: '2' }
+]
